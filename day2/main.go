340e1b248186ebd0ccfdbf142a551b1d80ad48ed
Nithin Jino <nithjino1995@gmail.com>
2024-12-02 22:52:41 -0500

    day2: removed debug print statements

--- day2/main.go
+++ day2/main.go
@@ -10,25 +10,16 @@ import (
 )
 
 func isSafe(levels []int) bool {
-	//scanIndex := 0
 	prevLevel := 0
 	prevDifference := 0
 	incrementSafeReport := true
-	//fmt.Printf("going to process %d\n", reports)
 	for index, level := range levels {
 		if index == 0 {
 			prevLevel = level
 		} else {
-			//fmt.Printf("prevLevel: %d\n", prevLevel)
-			//fmt.Printf("level: %d\n", level)
-
 			difference := level - prevLevel
-			//fmt.Printf("prevDifference: %d\n", prevDifference)
-			//fmt.Printf("difference: %d\n\n", difference)
 
 			if (index == 1 && difference == 0) || (index > 1 && !(difference != 0 && ((prevDifference < 0 && difference < 0) || (prevDifference > 0 && difference > 0)))) {
-				//fmt.Printf("%d is unsafe because the prevDifference and difference are different signs\nprevDifference: %d\ndifference: %d\n", reports, prevDifference, difference)
-				//fmt.Printf("current level: %d\nprevLevel: %d\n\n", level, prevLevel)
 				incrementSafeReport = false
 			} else {
 				prevDifference = difference
@@ -39,8 +30,6 @@ func isSafe(levels []int) bool {
 				difference *= -1
 			}
 			if difference < 1 || difference > 3 {
-				//fmt.Printf("%d is unsafe because of the difference is too big or 0.\ndifference: %d\n\n", reports, difference)
-				//fmt.Printf("current level: %d\nprevLevel: %d\n\n", level, prevLevel)
 				incrementSafeReport = false
 			}
 		}
@@ -72,13 +61,11 @@ func main() {
 			safeReports++
 		} else {
 			for i := range levels {
-				//fmt.Printf("pre deletelevels: %d\n", levels)
 				leftSide := make([]int, i)
 				rightSide := make([]int, len(levels)-len(leftSide)-1)
 				copy(leftSide, levels[:i])
 				copy(rightSide, levels[i+1:])
 				dampenedLevels := append(leftSide, rightSide...)
-				//fmt.Printf("levels: %d\nremoving at index %d: %d\ndamepenedLevels: %d\n\n", levels, i, n, dampenedLevels)
 				if isSafe(dampenedLevels) {
 					dampenedSafeReports++
 					break
