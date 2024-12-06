be232d1d23fe7f66a44b81fcd0cfb76bbde526c7
Nithin Jino <nithjino1995@gmail.com>
2024-12-06 11:44:50 -0500

    day5: cleaned up code

--- day5/main.go
+++ day5/main.go
@@ -13,6 +13,33 @@ func IgnoreError[T any](val T, err error) T {
 	return val
 }
 
+func checkRules(pages []string, rules [][]string) bool {
+	for _, rule := range rules {
+		x := rule[0]
+		y := rule[1]
+
+		xIndex := slices.Index(pages, x)
+		yIndex := slices.Index(pages, y)
+
+		if xIndex == -1 || yIndex == -1 {
+			continue
+		}
+
+		if xIndex > yIndex {
+			return false
+		}
+	}
+	return true
+}
+
+func processInputText(content []string, delim string) [][]string {
+	processed := [][]string{}
+	for _, c := range content {
+		processed = append(processed, strings.Split(c, delim))
+	}
+	return processed
+}
+
 func main() {
 	contentBytes, err := os.ReadFile("day5.txt")
 
@@ -21,31 +48,15 @@ func main() {
 	}
 
 	content := strings.Split(string(contentBytes), "\n\n")
-	rules := strings.Split(content[0], "\n")
-	pageNumberUpdates := strings.Split(content[1], "\n")
+
+	rules := processInputText(strings.Split(content[0], "\n"), "|")
+	pageNumberUpdates := processInputText(strings.Split(content[1], "\n"), ",")
+
 	middlePageSum := 0
+	correctedMiddlePageSum := 0
 
-	for _, pageNumberUpdate := range pageNumberUpdates {
-		passesRules := true
-		pages := strings.Split(pageNumberUpdate, ",")
-
-		for _, rule := range rules {
-			rulesSplt := strings.Split(rule, "|")
-			x := rulesSplt[0]
-			y := rulesSplt[1]
-
-			xIndex := slices.Index(pages, x)
-			yIndex := slices.Index(pages, y)
-
-			if xIndex == -1 || yIndex == -1 {
-				continue
-			}
-
-			if xIndex > yIndex {
-				passesRules = false
-				break
-			}
-		}
+	for _, pages := range pageNumberUpdates {
+		passesRules := checkRules(pages, rules)
 
 		if passesRules {
 			middlePageNum, err := strconv.Atoi(pages[len(pages)/2])
@@ -58,4 +69,5 @@ func main() {
 		}
 	}
 	fmt.Printf("\nDay 5 Part 1: %d\n", middlePageSum)
+	fmt.Printf("Day 5 Part 2: %d\n", correctedMiddlePageSum)
 }
