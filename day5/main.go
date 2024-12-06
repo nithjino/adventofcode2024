f0ce5f3d062c72ca7d82da6841161312ff2d4938
Nithin Jino <nithjino1995@gmail.com>
2024-12-06 16:07:32 -0500

    day5: finished part 2

--- day5/main.go
+++ day5/main.go
@@ -13,7 +13,7 @@ func IgnoreError[T any](val T, err error) T {
 	return val
 }
 
-func checkRules(pages []string, rules [][]string) bool {
+func checkRules(pages []string, rules [][]string) (bool, []string) {
 	for _, rule := range rules {
 		x := rule[0]
 		y := rule[1]
@@ -26,10 +26,10 @@ func checkRules(pages []string, rules [][]string) bool {
 		}
 
 		if xIndex > yIndex {
-			return false
+			return false, rule
 		}
 	}
-	return true
+	return true, []string{"-1", "-1"}
 }
 
 func processInputText(content string, delim string) [][]string {
@@ -54,9 +54,10 @@ func main() {
 
 	middlePageSum := 0
 	correctedMiddlePageSum := 0
+	failedCheckPages := [][][]string{}
 
 	for _, pages := range pageNumberUpdates {
-		passesRules := checkRules(pages, rules)
+		passesRules, failedRule := checkRules(pages, rules)
 
 		if passesRules {
 			middlePageNum, err := strconv.Atoi(pages[len(pages)/2])
@@ -66,8 +67,48 @@ func main() {
 			}
 
 			middlePageSum += middlePageNum
+		} else {
+			failedCheckPages = append(failedCheckPages, [][]string{pages, failedRule})
 		}
 	}
+
+	for _, failedPageSet := range failedCheckPages {
+		fmt.Println(failedPageSet)
+		failedPages := failedPageSet[0]
+		failedRule := failedPageSet[1]
+		failedX := failedRule[0]
+		failedY := failedRule[1]
+
+		failedXIndex := slices.Index(failedPages, failedX)
+		failedYIndex := slices.Index(failedPages, failedY)
+		failedPages[failedXIndex], failedPages[failedYIndex] = failedPages[failedYIndex], failedPages[failedXIndex]
+		passTest := false
+		passTest, failedRule = checkRules(failedPages, rules)
+
+		for !passTest {
+			failedX = failedRule[0]
+			failedY = failedRule[1]
+			failedXIndex = slices.Index(failedPages, failedX)
+			failedYIndex = slices.Index(failedPages, failedY)
+			fmt.Printf("Before swap: %s\n", failedPages)
+			fmt.Printf("failed rule: %s\n", failedRule)
+			fmt.Printf("failedXIndex: %d\n", failedXIndex)
+			fmt.Printf("failedYIndex: %d\n", failedYIndex)
+			failedPages[failedXIndex], failedPages[failedYIndex] = failedPages[failedYIndex], failedPages[failedXIndex]
+			fmt.Printf("After swap: %s\n", failedPages)
+			passTest, failedRule = checkRules(failedPages, rules)
+		}
+		fmt.Printf("\n%s now passes all rule checks\n", failedPages)
+		middlePageNum, err := strconv.Atoi(failedPages[len(failedPages)/2])
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		correctedMiddlePageSum += middlePageNum
+
+	}
+
 	fmt.Printf("\nDay 5 Part 1: %d\n", middlePageSum)
 	fmt.Printf("Day 5 Part 2: %d\n", correctedMiddlePageSum)
 }
